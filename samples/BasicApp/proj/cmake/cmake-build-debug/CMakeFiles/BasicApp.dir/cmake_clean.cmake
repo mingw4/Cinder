447f1366bb4c4a4821055043fc4945da4139c1c9
mingw4 <mingw4@illinois.edu>
2022-03-08 18:29:14 -0600

    Initial Commit

---
+++ samples/BasicApp/proj/cmake/cmake-build-debug/CMakeFiles/BasicApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BasicApp.dir/Users/MingWang/Cinder/samples/BasicApp/src/BasicApp.cpp.o"
+  "CMakeFiles/BasicApp.dir/Users/MingWang/Cinder/samples/BasicApp/src/BasicApp.cpp.o.d"
+  "Debug/BasicApp/BasicApp.app/Contents/MacOS/BasicApp"
+  "Debug/BasicApp/BasicApp.app/Contents/Resources/CinderApp.icns"
+  "Debug/BasicApp/BasicApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BasicApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
